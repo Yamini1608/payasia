d607670b6995452f76a84a3e3d536e8d08d9d7db
Sriram <sriram5052@gmail.com>
2018-05-14 07:49:38 +0530

    intitial commit

---
+++ payasiasample/payasiasample/Entities/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace payasiasample.Entities
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("data source=RAM;initial catalog=payasiasample;uid=sa;pwd=root;MultipleActiveResultSets=True;App=EntityFramework");
+        }
+    }
+}
